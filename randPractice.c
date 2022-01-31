1fd864f7aaec2c02a3161ae3b1a64f4948465a28
aporopa <92827108+aporopa@users.noreply.github.com>
2022-01-31 09:53:03 -0500

    Update randPractice.c

--- randPractice.c
+++ randPractice.c
@@ -6,7 +6,7 @@
 int main(void){
 
 
-srand((int)time(0));
+srand((int)time(0)); //f you decide to use seed for this instead of time decalre the seed independently
 
 printf("%d\n", rand());
 printf("%d\n", rand());
