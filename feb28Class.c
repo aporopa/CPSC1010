bb98580739d6c8da5132130eba8df9a9d9eb5d71
aporopa <92827108+aporopa@users.noreply.github.com>
2022-02-28 09:54:25 -0500

    Create feb28Class.c

---
+++ feb28Class.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main(void){
+
+    int size = 0;
+    int array[size];
+    int threshold;
+    int count = 0;
+    
+    //get data from user
+    printf("How many integers to enter?\n");
+    scanf("%d", &size);
+
+    printf("Please enter %d integers: ", size);
+    for(int i = 0; i < size; ++i){
+        scanf("%d", &array[i]);
+    }
+
+    printf("Enter threshold: \n");
+    scanf("%d", &threshold);
+
+    //get data for threshold
+    for(int i = 0; i < size; ++i){
+        if(array[i] <= threshold){ //checking one integer in the array at a time
+            if(count == 0) {
+            printf("%d", array[i]);
+            ++count; 
+            }
+            else{
+                printf(", %d", array[i]);
+            }
+        } 
+    }
+
+printf("\n");
+
+    return 0;
+}
