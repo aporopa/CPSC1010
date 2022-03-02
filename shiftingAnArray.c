4604413f237217ebda854eda53a0bfd032c03424
aporopa <92827108+aporopa@users.noreply.github.com>
2022-03-01 21:50:34 -0500

    Update shiftingAnArray.c

--- shiftingAnArray.c
+++ shiftingAnArray.c
@@ -9,11 +9,11 @@ int main(void) {
    for (i = 0; i < SCORES_SIZE; ++i) {
       scanf("%d", &(oldScores[i]));
    }
-
-newScores[SCORES_SIZE - 1] = oldScores[0];
+//assigning the last value of the new array with the first value of the old array
+newScores[SCORES_SIZE - 1] = oldScores[0]; 
 
    for (i = 0; i < SCORES_SIZE; ++i){
-      newScores[i-1] = oldScores[i];
+      newScores[i-1] = oldScores[i]; //the minus one without the brackets
    }
 
    for (i = 0; i < SCORES_SIZE; ++i) {
